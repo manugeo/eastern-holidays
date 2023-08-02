3579b3bbd4b0199112cb371176a8298fe67cae51
Manu <manugeo13@gmail.com>
2023-08-02 11:22:27 +0530

    Added custom google font.

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import Landing from "./_components/landing";
+import { Crimson_Pro } from 'next/font/google';
+
+const crimsonPro = Crimson_Pro({
+  subsets: ['latin'],
+  variable: '--font-crimson-pro'
+})
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen">
+    <main className={`flex min-h-screen ${crimsonPro.variable} font-crimson-pro`}>
       <div className="max-w-screen-sm h-full bg-slate-900 text-slate-50">
         <Landing />
       </div>
