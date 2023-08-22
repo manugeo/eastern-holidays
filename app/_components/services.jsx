e0565d03fcffcc82f2fc62559eecb48ec427bd3a
Manu <manugeo13@gmail.com>
2023-08-22 11:37:25 +0530

    Customer Details - Added service type select.

--- app/_components/services.jsx
+++ app/_components/services.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { SERVICES } from "@/lib/eastern-holidays";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 // Default theme
 import '@splidejs/react-splide/css';
@@ -10,119 +11,6 @@ import { useRouter } from "next/navigation";
 
 const Services = () => {
   const router = useRouter();
-  const SERVICES = [
-    {
-      name: 'Day Cruise',
-      ititnery: [
-        {
-          id: '01',
-          time: '11:00 am',
-          title: 'Checkin and houseboat cruise starts',
-          description: 'Welcome drinks and fruits are served while boarding the houseboat.'
-        },
-        {
-          id: '02',
-          time: '01:00 pm',
-          title: 'Lunch is served to the guests, we serve authentic Kerala food.',
-          description: 'Rice, severn types of vegetables and fish fry.'
-        },
-        {
-          id: '03',
-          time: '02:00 pm',
-          title: 'Cruise resumes.',
-          description: ''
-        },
-        {
-          id: '04',
-          time: '04:00 pm',
-          title: 'Tea & Coffee is served with snacks.',
-          description: ''
-        },
-        {
-          id: '05',
-          time: '05:30 pm',
-          title: 'Anchoring and checkout.',
-          description: ''
-        }
-      ]
-    },
-    {
-      name: 'Standard',
-      ititnery: [
-        {
-          id: '01',
-          time: '11:00 am',
-          title: 'Checkin and houseboat cruise starts',
-          description: 'Welcome drinks and fruits are served while boarding the houseboat.'
-        },
-        {
-          id: '02',
-          time: '01:00 pm',
-          title: 'Lunch is served to the guests, we serve authentic Kerala food.',
-          description: 'Rice, severn types of vegetables and fish fry.'
-        },
-        {
-          id: '03',
-          time: '02:00 pm',
-          title: 'Cruise resumes.',
-          description: ''
-        },
-        {
-          id: '04',
-          time: '04:00 pm',
-          title: 'Tea & Coffee is served with snacks.',
-          description: ''
-        },
-        {
-          id: '05',
-          time: '05:30 pm',
-          title: 'Anchoring',
-          description: ''
-        },
-        {
-          id: '06',
-          time: '08:00 pm',
-          title: 'Dinner is served.',
-          description: 'It has chapatti, chicken curry, dal curry, white rice and four types of vegetable dishes all cooked in Kerala style. Overnight stay in A/C deluxe rooms.'
-        },
-        {
-          id: '07',
-          time: '07:00 am',
-          title: 'Enjoy the morning in the houseboat followed by breakfast.',
-          description: 'It includes bread toast, Omlet, appam, idiappam, poori, egg curry, vegetable curry & steamed banana.'
-        },
-        {
-          id: '08',
-          time: '09:00 am',
-          title: 'Morning cruise and checkout.',
-          description: ''
-        },
-      ]
-    },
-    {
-      name: 'Night Stay',
-      ititnery: [
-        {
-          id: '01',
-          time: '08:00 pm',
-          title: 'Dinner is served.',
-          description: 'It has chapatti, chicken curry, dal curry, white rice and four types of vegetable dishes all cooked in Kerala style. Overnight stay in A/C deluxe rooms.'
-        },
-        {
-          id: '02',
-          time: '07:00 am',
-          title: 'Enjoy the morning in the houseboat followed by breakfast.',
-          description: 'It includes bread toast, Omlet, appam, idiappam, poori, egg curry, vegetable curry & steamed banana.'
-        },
-        {
-          id: '03',
-          time: '09:00 am',
-          title: 'Morning cruise and checkout.',
-          description: ''
-        },
-      ]
-    }
-  ]
   const splideOptions = {
     width: '100%',
     start: 1,
--- components/pages/customer-details.jsx
+++ components/pages/customer-details.jsx
@@ -1,7 +1,37 @@
+'use client'
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { SERVICES } from "@/lib/eastern-holidays";
+import { useState } from "react";
+
+const InputSelect = ({ value, onValueChange, placeholder = '', options = [] }) => {
+  return (
+    <Select value={value} onValueChange={onValueChange}>
+      <SelectTrigger>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+
+      <SelectContent>
+        {options.map(({ name, value }) => <SelectItem value={value} key={value}>
+          {name}
+        </SelectItem>)}
+      </SelectContent>
+    </Select>
+  );
+};
+
 const CustomerDetails = () => {
-  // Todo: Continue with Initial Implementation.
-  return(
-    <div className="max-w-screen-sm mx-auto h-full">Customer Details</div>
+  const [serviceType, setServiceType] = useState(SERVICES[1].id);
+  const serviceTypeOptions = SERVICES.map(({ name, id }) => ({ name, value: id }));
+  const onServiceTypeChange = (newServiceType) => setServiceType(newServiceType);;
+
+  return (
+    <div className="w-full max-w-screen-sm mx-auto flex flex-col justify-around min-h-screen">
+      <div className="p-4">
+        <InputSelect value={serviceType} onValueChange={onServiceTypeChange}
+          placeholder="Select the type of service that you need." options={serviceTypeOptions} />
+      </div>
+    </div>
   )
 };
 
