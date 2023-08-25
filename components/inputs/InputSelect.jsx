54fd0992637a38611453b014de1c73071683ddb0
Manu <manugeo13@gmail.com>
2023-08-25 12:14:18 +0530

    Created new Input Select component.

---
+++ components/inputs/InputSelect.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+const InputSelect = ({ label = '', value, onValueChange = () => {}, placeholder = '', options = [] }) => {
+  return (
+    <div>
+      {label && <p className="py-2">{label}</p>}
+      <Select value={value} onValueChange={onValueChange}>
+        <SelectTrigger>
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+
+        <SelectContent>
+          {options.map(({ name, value }) => <SelectItem value={value} key={value}>
+            {name}
+          </SelectItem>)}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default InputSelect;
--- components/pages/customer-details.jsx
+++ components/pages/customer-details.jsx
@@ -1,24 +1,8 @@
 'use client'
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { SERVICES } from "@/lib/eastern-holidays";
 import { useState } from "react";
-
-const InputSelect = ({ value, onValueChange, placeholder = '', options = [] }) => {
-  return (
-    <Select value={value} onValueChange={onValueChange}>
-      <SelectTrigger>
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-
-      <SelectContent>
-        {options.map(({ name, value }) => <SelectItem value={value} key={value}>
-          {name}
-        </SelectItem>)}
-      </SelectContent>
-    </Select>
-  );
-};
+import InputSelect from "../inputs/InputSelect";
 
 const CustomerDetails = () => {
   const [serviceType, setServiceType] = useState(SERVICES[1].id);
@@ -28,7 +12,7 @@ const CustomerDetails = () => {
   return (
     <div className="w-full max-w-screen-sm mx-auto flex flex-col justify-around min-h-screen">
       <div className="p-4">
-        <InputSelect value={serviceType} onValueChange={onServiceTypeChange}
+        <InputSelect label="Type of service." value={serviceType} onValueChange={onServiceTypeChange}
           placeholder="Select the type of service that you need." options={serviceTypeOptions} />
       </div>
     </div>
