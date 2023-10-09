545cce5f3b47b497127db664dc65fe1c262548e5
Manu <manugeo13@gmail.com>
2023-10-09 19:43:28 +0530

    Renamed 'Home' to 'home-page'.

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
-import Home from "@/components/pages/Home";
+import Home from "@/components/pages/home-page";
 
 export default function Page() {
-  return (
-    <Home />
-  )
+  return <Home />;
 }
