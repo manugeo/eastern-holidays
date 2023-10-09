74960dca3046cd713ee28f1ef96cd4955db7dbfe
Manu <manugeo13@gmail.com>
2023-10-09 19:50:52 +0530

    Updated main title section content.

--- components/pages/home.jsx
+++ components/pages/home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
       <ImageSplider />
 
       <div className="mt-8 mx-6">
-        <TextH3 className=''>Eastern Holidays</TextH3>
-        <TextLarge className="mt-3">Premium house boat in Alappuzha, Kerala</TextLarge>
+        <TextH3 className=''>Discover Eastern Holidays</TextH3>
+        <TextLarge className="mt-3">Your Gateway to Houseboat Escapes in Alappuzha, Kerala</TextLarge>
         <TextP className="mt-1 text-sm">8 guests &#x2022; 3 bedrooms &#x2022; 2 bathrooms</TextP>
       </div>
 
