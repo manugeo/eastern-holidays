37ae578c086f1dc43be164801598c67418a2467b
Manu <manugeo13@gmail.com>
2023-10-09 18:44:43 +0530

    Minor change.

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,5 @@ import Home from "@/components/pages/Home";
 export default function Page() {
   return (
     <Home />
-    // <div className="max-w-screen-sm mx-auto h-full bg-slate-50"></div>
   )
 }
