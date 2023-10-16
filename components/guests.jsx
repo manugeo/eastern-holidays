75300ddf5c86e20418d4a1ce9c2185fc1814b00c
Manu <manugeo13@gmail.com>
2023-10-16 15:18:23 +0530

    Prices page - Added Gusests section.

---
+++ components/guests.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { Minus, Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { TextH4, TextMuted, TextSmall } from "./ui/texts";
+import { useState } from "react";
+
+const Guests = ({ className }) => {
+  const [adults, setAdults] = useState(4);
+  const [children, setChildren] = useState(0);
+
+  return (
+    <div className={className}>
+      <TextH4>Guests</TextH4>
+
+      <div className="flex justify-between mt-3">
+        <div>
+          <TextSmall>Adults</TextSmall>
+          <TextMuted>Age 7+</TextMuted>
+        </div>
+        <div className="basis-[120px] flex justify-between items-center">
+          <Button variant="outline" size="icon" disabled={(adults <= 2)} onClick={() => setAdults(adults - 1)}><Minus /></Button>
+          <TextSmall>{adults}</TextSmall>
+          <Button variant="outline" size="icon" disabled={(adults >= 8)} onClick={() => setAdults(adults + 1)}><Plus /></Button>
+        </div>
+      </div>
+
+      <div className="flex justify-between mt-2">
+        <div>
+          <TextSmall>Children</TextSmall>
+          <TextMuted>Under 7</TextMuted>
+        </div>
+        <div className="basis-[120px] flex justify-between items-center">
+          <Button variant="outline" size="icon" disabled={(children <= 0)} onClick={() => setChildren(children - 1)}><Minus /></Button>
+          <TextSmall>{children}</TextSmall>
+          <Button variant="outline" size="icon" disabled={(children >= 6)} onClick={() => setChildren(children + 1)}><Plus /></Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Guests;
--- components/pages/prices.jsx
+++ components/pages/prices.jsx
@@ -1,11 +1,13 @@
+import Guests from "../guests";
 import Navbar from "../navbar";
 import PriceCalendar from "../price-calendar";
 
 const Prices = () => {
   return (
-    <div className="flex flex-col">
-      <Navbar className={'mt-4 mx-6'} />
-      <PriceCalendar className={"mt-6 mx-6"} />
+    <div className="px-6 flex flex-col">
+      <Navbar className={'mt-4'} />
+      <PriceCalendar className={"mt-6"} />
+      <Guests className={"mt-6"} />
     </div>
   );
 };
