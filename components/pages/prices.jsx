787466500107583dd5f8785e79bf7f5a1030c12e
Manu <manugeo13@gmail.com>
2023-10-18 16:14:45 +0530

    Prices page - Further updates to Verify Number section.

--- components/pages/prices.jsx
+++ components/pages/prices.jsx
@@ -1,19 +1,26 @@
+'use client'
 
+import { useState } from "react";
 import Guests from "../guests";
 import Navbar from "../navbar";
 import PriceCalendar from "../price-calendar";
 import PriceSummary from "../price-summary";
 import Separator from "../ui/separator";
+import VerifyNumber from "../verify-number";
 
 const Prices = () => {
+  // Note: This number should not include country code.
+  const [number, setNumber] = useState('');
+
   return (
-    <div className="px-6 flex flex-col">
+    <div className="px-6 pb-6 flex flex-col">
       <Navbar className={'mt-4'} />
       <PriceCalendar className={'mt-6'} />
       <Guests className={'mt-6'} />
       <Separator className="mt-6" />
       <PriceSummary className={'mt-6'} />
       <Separator className="mt-6" />
+      <VerifyNumber className={'mt-6'} number={number} setNumber={setNumber} />
     </div>
   );
 };
---
+++ components/verify-number.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import InputText from "./inputs/input-text";
+import { Button } from "./ui/button";
+import { TextH4 } from "./ui/texts";
+import { getNumberWithCountryCode, getNumberWithoutCountryCode } from "@/lib/utils";
+
+const VerifyNumber = ({ className, number = null, setNumber = () => { } }) => {
+  const [displayNumber, setDisplayNumber] = useState(number ? getNumberWithCountryCode(number) : '');
+
+
+  const onChange = (e) => {
+    const newNumber = getNumberWithoutCountryCode(e.target.value);
+    setNumber(newNumber);
+    const newDisplayNumber = getNumberWithCountryCode(newNumber);
+    setDisplayNumber(newDisplayNumber);
+  };
+  const onFocus = (e) => {
+    console.log('On focus:', e);
+    const currentValue = e.target.value;
+    if (currentValue === '') setDisplayNumber('+91 ');
+  };
+  const onBlur = (e) => {
+    console.log('On blur:', e);
+    const currentValue = e.target.value;
+    if (currentValue === '+91 ') setDisplayNumber('');
+  };
+
+  return (
+    <div className={className}>
+      <TextH4>Verify your number to book</TextH4>
+      <InputText className="mt-3" inputClassName="text-sm font-medium leading-none" label="Phone Number" id="phone-number" type="tel"
+        placeholder="Enter your 10-digit phone number" helperText="We'll text you to confirm your number. Only Indian numbers are supported for now."
+        value={displayNumber} onChange={onChange} onFocus={onFocus} onBlur={onBlur} />
+      <Button className="mt-4 w-full">Continue</Button>
+    </div>
+  );
+};
+
+export default VerifyNumber;
