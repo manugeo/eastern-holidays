edf4b2032bb0739ef0615895f068e3da494d382a
Manu <manugeo13@gmail.com>
2023-10-30 17:25:46 +0530

    Updates to Price Summary component.

--- components/price-summary.jsx
+++ components/price-summary.jsx
@@ -1,12 +1,22 @@
+import { format } from "date-fns";
 import PriceTable from "./price-table";
 import { TextH4, TextSmall } from "./ui/texts";
+import { getTotalPrice } from "@/lib/utils";
+import { useMemo } from "react";
 
-const PriceSummary = ({ className }) => {
+const PriceSummary = ({ guests, travelDate, className }) => {
+  const getTotalGuests = () => guests ? guests.adults + guests.children + guests.infants : 0;
+  const getTravelDateString = () => travelDate ? format(travelDate, 'EEEE, d MMMM, yyyy') : '';
+  const totalPrice = useMemo(() => getTotalPrice(guests, travelDate), [guests, travelDate]);
+  const payableNow = useMemo(() => totalPrice * 0.2, [totalPrice]);
+
   return (
     <div className={className}>
       <TextH4>Summary</TextH4>
-      <TextSmall>Your Booking Summary for 7 Guests on Wednesday, 25 October, 2023</TextSmall>
-      <PriceTable />
+      <TextSmall>
+        {`Your Booking Summary for ${getTotalGuests()} Guests on ${getTravelDateString()}`}
+      </TextSmall>
+      <PriceTable totalPrice={totalPrice} payableNow={payableNow} />
     </div>
   );
 };
--- components/price-table.jsx
+++ components/price-table.jsx
@@ -1,15 +1,16 @@
+import { numberWithCommas } from "@/lib/utils";
 import { Table, TableBody, TableCell, TableRow } from "./ui/table";
-const PriceTable = () => {
+const PriceTable = ({ totalPrice = 0, payableNow = 0 }) => {
   return (
     <Table className="mt-3">
         <TableBody>
           <TableRow>
             <TableCell className="font-medium">Total</TableCell>
-            <TableCell className="text-right">₹10,000</TableCell>
+            <TableCell className="text-right">{`₹${numberWithCommas(totalPrice)}`}</TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">Payable Now</TableCell>
-            <TableCell className="text-right">₹2,000</TableCell>
+            <TableCell className="text-right">{`₹${numberWithCommas(payableNow)}`}</TableCell>
           </TableRow>
         </TableBody>
       </Table>
