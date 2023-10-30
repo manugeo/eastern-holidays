62c9e96c5538bc2dcb4f393bab6ab9adc077a204
Manu <manugeo13@gmail.com>
2023-10-30 15:32:13 +0530

    Updated Guests component.

--- components/guests.jsx
+++ components/guests.jsx
@@ -3,12 +3,53 @@
 import { Minus, Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import { TextH4, TextMuted, TextSmall } from "./ui/texts";
-import { useState } from "react";
+import { useCallback, useReducer } from "react";
+import { DEFAULT_GUESTS } from "@/lib/eastern-holidays";
 
-const Guests = ({ className }) => {
-  const [adults, setAdults] = useState(4);
-  const [children, setChildren] = useState(0);
-  const [infants, setInfants] = useState(0);
+// Reducer function to update the state based on different actions
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_ADULTS":
+      return { ...state, adults: action.payload };
+    case "UPDATE_CHILDREN":
+      return { ...state, children: action.payload };
+    case "UPDATE_INFANTS":
+      return { ...state, infants: action.payload };
+    default:
+      return state;
+  }
+};
+
+/**
+ * Guests component that displays the number of adults, children, and infants.
+ * @param {Object} props - The component props.
+ * @param {Object} props.guests - The initial guests object.
+ * @param {Function} props.onGuestsChange - The callback function to handle guest changes.
+ * @param {string} props.className - The CSS class name for the component.
+ */
+const Guests = ({ guests = null, onGuestsChange = () => { }, className }) => {
+  const [guestState, dispatch] = useReducer(reducer, (guests || DEFAULT_GUESTS));
+
+  const updateGuests = useCallback((guestType, newValue) => {
+    const { adults, children, infants } = guestState;
+
+    switch (guestType) {
+      case "adults":
+        onGuestsChange({ adults: newValue, children: children, infants: infants });
+        dispatch({ type: "UPDATE_ADULTS", payload: newValue });
+        break;
+      case "children":
+        onGuestsChange({ adults: adults, children: newValue, infants: infants });
+        dispatch({ type: "UPDATE_CHILDREN", payload: newValue });
+        break;
+      case "infants":
+        onGuestsChange({ adults: adults, children: children, infants: newValue });
+        dispatch({ type: "UPDATE_INFANTS", payload: newValue });
+        break;
+    }
+  }, [guestState, onGuestsChange]);
+
+  const { adults, children, infants } = guestState;
 
   return (
     <div className={className}>
@@ -20,9 +61,9 @@ const Guests = ({ className }) => {
           <TextMuted>Age 13+</TextMuted>
         </div>
         <div className="basis-[120px] flex justify-between items-center">
-          <Button variant="outline" size="icon" disabled={(adults <= 2)} onClick={() => setAdults(adults - 1)}><Minus /></Button>
+          <Button variant="outline" size="icon" disabled={(adults <= 2)} onClick={() => updateGuests('adults', adults - 1)}><Minus /></Button>
           <TextSmall>{adults}</TextSmall>
-          <Button variant="outline" size="icon" disabled={(adults >= 8)} onClick={() => setAdults(adults + 1)}><Plus /></Button>
+          <Button variant="outline" size="icon" disabled={(adults >= 8)} onClick={() => updateGuests('adults', adults + 1)}><Plus /></Button>
         </div>
       </div>
 
@@ -32,9 +73,9 @@ const Guests = ({ className }) => {
           <TextMuted>Ages 2-12</TextMuted>
         </div>
         <div className="basis-[120px] flex justify-between items-center">
-          <Button variant="outline" size="icon" disabled={(children <= 0)} onClick={() => setChildren(children - 1)}><Minus /></Button>
+          <Button variant="outline" size="icon" disabled={(children <= 0)} onClick={() => updateGuests('children', children - 1)}><Minus /></Button>
           <TextSmall>{children}</TextSmall>
-          <Button variant="outline" size="icon" disabled={(children >= 6)} onClick={() => setChildren(children + 1)}><Plus /></Button>
+          <Button variant="outline" size="icon" disabled={(children >= 6)} onClick={() => updateGuests('children', children + 1)}><Plus /></Button>
         </div>
       </div>
 
@@ -44,9 +85,9 @@ const Guests = ({ className }) => {
           <TextMuted>Under 2</TextMuted>
         </div>
         <div className="basis-[120px] flex justify-between items-center">
-          <Button variant="outline" size="icon" disabled={(infants <= 0)} onClick={() => setInfants(infants - 1)}><Minus /></Button>
+          <Button variant="outline" size="icon" disabled={(infants <= 0)} onClick={() => updateGuests('infants', infants - 1)}><Minus /></Button>
           <TextSmall>{infants}</TextSmall>
-          <Button variant="outline" size="icon" disabled={(infants >= 6)} onClick={() => setInfants(infants + 1)}><Plus /></Button>
+          <Button variant="outline" size="icon" disabled={(infants >= 6)} onClick={() => updateGuests('infants', infants + 1)}><Plus /></Button>
         </div>
       </div>
     </div>
