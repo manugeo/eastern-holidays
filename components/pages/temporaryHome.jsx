bde75c5335c6490a716d86f2ac7250dc04e0ec82
Manu <manugeo13@gmail.com>
2024-01-29 23:35:37 +0530

    Further updates to temp home page.

--- components/pages/temporaryHome.jsx
+++ components/pages/temporaryHome.jsx
@@ -1,16 +1,17 @@
-import { TextH3, TextLarge, TextP } from "../ui/texts";
+import { TextH2, TextLarge, TextP } from "../ui/texts";
 import Separator from "../ui/separator";
 import { ModeToggle } from "../mode-toggle";
 
 const TemporaryHome = () => {
   return (
-    <div className="min-h-screen pb-6 flex flex-col justify-center">
-      <div className="mt-6 mx-6">
-        <TextH3 className=''>Exciting Things Are Coming!</TextH3>
-        <TextLarge className="mt-3">Stay tuned for updates on our houseboat booking platform in Alappuzha, Kerala.</TextLarge>
-        <TextP className="mt-1 text-sm">Explore and book unique houseboat experiences soon!</TextP>
+    <div className="min-h-screen pb-6 flex flex-col justify-center items-center">
+      <div className="text-center mx-6">
+        <TextH2 className=''>Welcome to <span className="text-primary whitespace-nowrap">Book A Boat - IN</span></TextH2>
+        <TextLarge className="mt-3">Exciting Things Are Coming!</TextLarge>
+        <TextP className="mt-1 text-sm">Stay tuned for updates on our houseboat booking platform in Alappuzha, Kerala.</TextP>
       </div>
       <Separator className="mt-6 mx-6" />
+      <ModeToggle />
     </div>
   );
 };
